1b3032a2db5168741db12aa6d7dbc20244a3b53b
Ali Al Dobyan <ali.aldobyan@gmail.com>
2020-10-07 13:10:20 +0300

    on click the cards flip vertically

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactCardFlip from "react-card-flip";
 
-const Card = (props) => {
+import cardBack from "../images/CardBack.jpg";
+
+const Card = ({ card, checkFlipped }) => {
+  const [flipped, setFlipped] = useState(false);
+
+  const handleFlip = () => {
+    setFlipped(!flipped);
+  };
   return (
     <div className="col-3 my-1">
-      <img
-        className="mx-auto"
-        src={props.card.front}
-        height="100%"
-        width="100%"
-        key="front"
-      />
+      <ReactCardFlip isFlipped={flipped} flipDirection="vertical">
+        <img
+          className="mx-auto"
+          src={cardBack}
+          height="100%"
+          width="100%"
+          key="front"
+          onClick={() => handleFlip()}
+        />
+        <img
+          className="mx-auto"
+          src={card.front}
+          height="100%"
+          width="100%"
+          key="back"
+          onClick={() => handleFlip()}
+        />
+      </ReactCardFlip>
     </div>
   );
 };
